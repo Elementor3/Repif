7c9f66d80037baaffb2e3df441e88568d6cc3680
Oleksandr Cherepkov <oleksandrcherepkov@gmail.com>
2026-05-22 14:08:29 +0000

    added favicons

--- www/public/auth/verify_email.php
+++ www/public/auth/verify_email.php
@@ -17,6 +17,7 @@ if ($token !== '') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= t('email_verification') ?> - WeatherStation</title>
+    <link rel="icon" type="image/svg+xml" href="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%230d6efd'><path d='M11.473 9.932a.5.5 0 0 1 .472.662 3 3 0 0 1-2.943 1.906H4.5a2.5 2.5 0 0 1-.09-4.998 3.5 3.5 0 0 1 6.892.144.5.5 0 0 1-.5.516h-.114a2 2 0 1 1-.215 3.97Z'/><path d='M7 0a.5.5 0 0 1 .5.5V2a.5.5 0 0 1-1 0V.5A.5.5 0 0 1 7 0Zm6.354 2.146a.5.5 0 0 1 0 .708l-1.061 1.06a.5.5 0 1 1-.708-.707l1.06-1.061a.5.5 0 0 1 .709 0ZM14 6.5a.5.5 0 0 1 .5.5v.5a.5.5 0 0 1-1 0V7a.5.5 0 0 1 .5-.5Zm-7.5-1a.5.5 0 0 1 0 1 2.5 2.5 0 0 0-2.5 2.5.5.5 0 0 1-1 0 3.5 3.5 0 0 1 3.5-3.5Z'/></svg>">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
